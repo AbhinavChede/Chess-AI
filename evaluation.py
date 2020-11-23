f9458e87ab206a7db09292dacf4fb885b61adb31
sudhikshal <74062358+sudhikshal@users.noreply.github.com>
2020-11-22 18:39:07 -0700

    added king protection and pawn structure

--- evaluation.py
+++ evaluation.py
@@ -9,13 +9,140 @@ class Evaluation:
             score += (len(board.pieces(piece, color)) - len(board.pieces(piece, not color))) * value
         return score
 
-    def kingProtection(self, board, color):
-        ourKing = board.king(color)
-        enemyKing = board.king(not color)
-        pass
+    def kingProtection(board, color):
+        def isFree(x, y):
+            return board[y][x] == ' '
 
-    def pawnStructure(self, board, color):
-        pass
+        def getColor(x, y):
+            if board[y][x] == ' ':
+                return ' '
+            elif board[y][x].isupper():
+                return 'w'
+            elif board[y][x].islower():
+                return 'b'
+
+        def isThreatened(board,color, lx, ly):
+            if color == 'w':
+                if lx < 7 and ly > 0 and board[ly - 1][lx + 1] == 'p':
+                    return True
+                elif lx > 0 and ly > 0 and board[ly - 1][lx - 1] == 'p':
+                    return True
+            else:
+                if lx < 7 and ly < 7 and board[ly + 1][lx + 1] == 'P':
+                    return True
+                elif lx > 0 and ly < 7 and board[ly + 1][lx - 1] == 'P':
+                    return True
+            m = [(lx + 1, ly + 2), (lx + 2, ly + 1), (lx + 2, ly - 1), (lx + 1, ly - 2),
+                    (lx - 1, ly + 2), (lx - 2, ly + 1), (lx - 1, ly - 2), (lx - 2, ly - 1)]
+            for p in m:
+                if p[0] >= 0 and p[0] <= 7 and p[1] >= 0 and p[1] <= 7:
+                    if board[p[1]][p[0]] == "n" and color == 'w':
+                        return True
+                    elif board[p[1]][p[0]] == "N" and color == 'b':
+                        return True
+            dirs = [(1, 0), (-1, 0), (0, 1), (0, -1),
+                    (1, 1), (-1, 1), (1, -1), (-1, -1)]
+            for d in dirs:
+                x = lx
+                y = ly
+                dx, dy = d
+                steps = 0
+                while True:
+                    steps += 1
+                    x += dx
+                    y += dy
+                    if x < 0 or x > 7 or y < 0 or y > 7:
+                        break
+                    if isFree(x, y):
+                        continue
+                    elif getColor(x, y) == color:
+                        break
+                    else:
+                        p = board[y][x].upper()
+                        if p == 'K' and steps == 1:
+                            return True
+                        elif p == 'Q':
+                            return True
+                        elif p == 'R' and abs(dx) != abs(dy):
+                            return True
+                        elif p == 'B' and abs(dx) == abs(dy):
+                            return True
+                        break
+            return False
+        def getKingLocation(board, color):
+            for y in range(0,8):
+                for x in range(0,8):
+                    if board[y][x] == "K" and color == 'w':
+                        return (x,y)
+                    if board[y][x] == "k" and color == 'b':
+                        return (x,y)
+
+        kx, ky = getKingLocation(board, color)
+        return isThreatened(board, color, kx, ky)
+
+    def pawnStructure(board, color):
+        my_pos = ()
+        was_enemy = False
+
+        def get_my_pos():
+            return my_pos
+
+        def get_color():
+            return color
+
+        def make_move(board, move):
+            if board[move[0]][move[1]] != None:
+                was_enemy = False
+            board[my_pos[0]][my_pos[1]] = None
+
+        def in_bounds(self, board, new_pos):
+
+            if was_enemy:
+                self.was_enemy = False
+                return False
+
+            # make sure 'new_pos' in inside the board
+            if new_pos[0] < 0 or new_pos[1] < 0:
+                return False
+            try:
+                item = board[new_pos[0]][new_pos[1]]
+                # item can only be an enemy
+                if item == None:
+                    return True
+                elif item.get_color() == self.get_color():
+                    return False
+                elif item.get_color() != self.get_color:
+                    self.was_enemy = True
+                    return True
+                else:
+                    return True
+            except:
+                return False
+        is_first_move = True
+        moves = []
+        my_pos = super(pawnStructure).get_my_pos()
+        if color == 'White':
+            if is_first_move:
+                is_first_move = False
+                new_pos = (my_pos[0] - 2, my_pos[1])
+                if super(pawnStructure).in_bounds(board, new_pos):
+                    moves.append(new_pos)
+            new_pos = (my_pos[0] - 1, my_pos[1])
+            if super(pawnStructure).in_bounds(board, new_pos):
+                moves.append(new_pos)
+
+        else:
+            if is_first_move:
+                is_first_move = False
+                new_pos = (my_pos[0] + 2, my_pos[1])
+                if super(pawnStructure).in_bounds(board, new_pos):
+                    moves.append(new_pos)
+            new_pos = (my_pos[0] + 1, my_pos[1])
+            if super(pawnStructure).in_bounds(board, new_pos):
+                moves.append(new_pos)
+
+        return moves
+
 
     def pieceDevelopment(self, board, color):
         king_developmentTable = [[-0.03, -0.04, -0.04, -0.05, -0.05, -0.04, -0.04, -0.03], 
