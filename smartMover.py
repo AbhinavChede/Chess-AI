b59c1c7a8f2e13d954b3b874c27b3061ec91eb59
AbhinavChede <74007926+AbhinavChede@users.noreply.github.com>
2020-12-01 08:21:38 -0700

    Add files via upload

--- smartMover.py
+++ smartMover.py
@@ -11,7 +11,11 @@ class Player:
         pass
         
     def move(self, board, time): 
+<<<<<<< HEAD
+        return self.minimax(board, 6, -1 * float('inf'), float('inf'), self.color)[0]
+=======
         return self.minimax(board, 2, -1 * float('inf'), float('inf'), self.color)[0]
+>>>>>>> 08b55fbf51465aca023168c65c70fc91a47fa473
 
     # Searches through the game tree to find an optimal move
     def minimax(self, board, depth, alpha, beta, maximizing_player):
@@ -24,7 +28,8 @@ class Player:
             max_eval = -1 * float('inf')
             for legalMove in legalMoves:
                 board.push(legalMove)
-                current_eval = self.minimax(board, depth - 1, alpha, beta, not self.color)[1]
+                # current_eval = self.minimax(board, depth - 1, alpha, beta, not self.color)[1]
+                current_eval = self.quiescence(board, self.color, alpha, beta)
                 board.pop()
                 if current_eval > max_eval:
                     max_eval = current_eval
@@ -37,7 +42,8 @@ class Player:
             min_eval = float('inf')
             for legalMove in legalMoves:
                 board.push(legalMove)
-                current_eval = self.minimax(board, depth - 1, alpha, beta, self.color)[1]
+                # current_eval = self.minimax(board, depth - 1, alpha, beta, self.color)[1]
+                current_eval = self.quiescence(board, not self.color, alpha, beta)
                 board.pop()
                 if current_eval < min_eval:
                     min_eval = current_eval
@@ -48,5 +54,29 @@ class Player:
             return best_move, min_eval
         if depth == 0 or len(legalMoves) == 0:
             return None, eval.finalEvaluation(board, self.color)
+
+    def quiescence(self, board, color, alpha, beta):
+        eval = Evaluation()
+        e = eval.finalEvaluation(board, color)
+        if e > beta:
+            return beta
+        if alpha < e:
+            alpha = e
+        legalMoves = board.legal_moves
+        score = 0
+        for legalMove in legalMoves:
+            numPieces = eval.pieceDifference(board, color)
+            board.push(legalMove)
+            newNumPieces = eval.pieceDifference(board, color)
+            if numPieces > newNumPieces + 0.01 or numPieces < newNumPieces - 0.01:
+                score = -1 * self.quiescence(board, color, alpha, beta)
+            board.pop()
+            if score >= beta:
+                return beta
+            if score >= alpha:
+                alpha = score
+        return alpha
+            
+
 
                     
