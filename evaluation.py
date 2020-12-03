cd43d7bb3e2029762b2d6bfff48504347c176677
AbhinavChede <74007926+AbhinavChede@users.noreply.github.com>
2020-12-03 08:24:14 -0700

    Add files via upload

--- evaluation.py
+++ evaluation.py
@@ -5,15 +5,22 @@ class Evaluation:
     """
         This class calculates useful information to use in th evaluation function.
     """
-
+        
     # Calculates the piece difference of both sides with weights that calculated, assigned, and adjusted to fit in a 0 to 1 scale.
     def pieceDifference(self, board, color):
+        self.poseval = {"P":[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 1.0, -2.0, -2.0, 1.0, 1.0, 0.5, 0.5, -0.5, 1.0, 0.0, 0.0, -1.0, -0.5, 0.5, 0.0, 0.0, 0.0, 2.0, 2.0, 0.0, 0.0, 0.0, 0.5, 0.5, 1.0, 2.5, 2.5, 1.0, 0.5, 0.5, 1.0, 1.0, 2.0, 3.0, 3.0, 2.0, 1.0, 1.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], 
+            "N":[-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0, -4.0, -2.0, 0.0, 0.5, 0.5, 0.0, -2.0, -4.0, -3.0, 0.5, 1.0, 1.5, 1.5, 1.0, 0.5, -3.0, -3.0, 0.0, 1.5, 2.0, 2.0, 1.5, 0.0, -3.0, -3.0, 0.5, 1.5, 2.0, 2.0, 1.5, 0.5, -3.0, -3.0, 0.0, 1.0, 1.5, 1.5, 1.0, 0.0, -3.0, -4.0, -2.0, 0.0, 0.0, 0.0, 0.0, -2.0, -4.0, -5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0], 
+            "B":[-2, -1, -1, -1, -1, -1, -1, -2, -1, 0.5, 0, 0, 0, 0, 0.5, -1,  -1, 1, 1, 1, 1, 1, 1, -1, -1, 0, 1, 1, 1, 1, 0, -1, -1, 0.5, 0.5, 1, 1, 0.5, 0.5, -1, -1, 0, 0.5, 1, 1, 0.5, 0, -1, -1, 0, 0, 0, 0, 0, 0, -1, -2, -1, -1, -1, -1, -1, -1, -2],
+            "R":[0, 0, 0, 0.5, 0.5, 0, 0, 0, -.5, 0, 0, 0, 0, 0, 0, -.5,-.5, 0, 0, 0, 0, 0, 0, -.5,-.5, 0, 0, 0, 0, 0, 0, -.5,-.5, 0, 0, 0, 0, 0, 0, -.5,-.5, 0, 0, 0, 0, 0, 0, -.5, .5, 1, 1, 1, 1, 1, 1, .5, 0, 0, 0, 0, 0, 0, 0, 0 ],
+            "Q":[-2, -1, -1, -.5, -.5, -1, -1, -2, -1, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, -1, -1, .5, .5, .5, .5, .5, 0.0, -1, 0, 0, 0.5, .5, .5, .5, 0.0, -.5, -.5, 0, .5, .5, .5, .5, 0, -.5, -1, 0, .5, .5, .5, .5, 0, -1, -1, 0, 0, 0, 0, 0, 0, -1, -2, -1, -1, -.5, -.5, -1, -1, -2],
+            "K":[2, 3, 1, 0, 0, 1, 3, 2, 2, 2, 0, 0, 0, 0, 2, 2, -1, -2, -2, -2, -2, -2, -2, -1,-2, -3, -3, -4, -4, -3, -3, -2, -3, -4, -4, -5, -5, -4, -4, -3,-3, -4, -4, -5, -5, -4, -4, -3,-3, -4, -4, -5, -5, -4, -4, -3,-3, -4, -4, -5, -5, -4, -4, -3]}
         score = 0
         for (piece, value) in [(chess.PAWN, 0.004448), (chess.BISHOP, 0.01468), (chess.KING, 0.88968),\
                             (chess.QUEEN, 0.040036), (chess.KNIGHT, 0.014235), (chess.ROOK, 0.022242)]:
             score += (len(board.pieces(piece, color)) - len(board.pieces(piece, not color))) * value
         return score
 
+    # Calculates the difference in pawn islands of both sides to figure out who has better pawn structure
     def pawnStructure(self, board, color):
         ourPawns = list(board.pieces(chess.PAWN, color))
         enemyPawns = list(board.pieces(chess.PAWN, not color))
@@ -94,9 +101,9 @@ class Evaluation:
         ourDevelopment = 0
         enemyDevelopment = 0
         for square in range(64):
-            if board.color_at(square) == color:
+            if board.color_at(square) == color and color:
                 ourDevelopment += developmentTables[6 - board.piece_type_at(square)][7 - int((square - square % 8)/8)][square % 8]
-            elif board.color_at(square) and board.color_at(square) != color:
+            elif board.color_at(square) and board.color_at(square) != color and not color:
                 enemyDevelopment += developmentTables[6 - board.piece_type_at(square)][int((square - square % 8)/8)][square % 8]
         return ourDevelopment - enemyDevelopment
 
@@ -110,10 +117,38 @@ class Evaluation:
             oppositeControl += len(list(board.attackers(not color, num)))
         return ownControl - oppositeControl
 
+    def incrEval(self, oldBoard, newBoard, move, prevEval, color):
+        incr = 0
+        mateval = [0,10,30,30,50,90,900]
+        if (newBoard.is_game_over()):
+            if (newBoard.is_variant_draw()):
+                return 0
+            if (newBoard.turn == color):
+                return -float("inf")
+            else:
+                return float("inf")
+        fromPiece = oldBoard.piece_at(move.from_square)
+        toPiece = oldBoard.piece_at(move.to_square)
+        if fromPiece.color == color:
+            if toPiece: #enemy piece was taken, add mateval
+                incr += mateval[toPiece.piece_type]
+            #subtract previous piece poseval and add new poseval
+            incr -= self.poseval[fromPiece.symbol().upper()][move.from_square]
+            incr += self.poseval[fromPiece.symbol().upper()][move.to_square]
+            if move.promotion: # a friendly promotion happened
+                incr += mateval[move.promotion]
+        else:
+            if toPiece: #friendly piece taken, subtract mateval and poseval
+                incr -= mateval[toPiece.piece_type]
+                incr -= self.poseval[toPiece.symbol().upper()][move.to_square]            
+            if move.promotion: # an enemy promotion happened
+                incr -= mateval[move.promotion]
+        return prevEval+incr        
+
     # Adds up all features to calculate a final evaluation
-    def finalEvaluation(self, board, color):
+    def finalEvaluation(self, board, color): # oldBoard, newBoard, prevEval, move):
         weights = [1.00, 1.00, 0.05, 0.05]
         result = weights[0] * self.pieceDifference(board, color) + weights[1] * self.pieceDevelopment(board, color) + \
                  weights[2] * self.centerControl(board, color) + weights[3] * self.pawnStructure(board, color)
-        return result
+        return result # + self.incrEval(oldBoard, newBoard, move, prevEval, color)
 
